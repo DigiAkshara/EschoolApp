4eb1294054bfc0d820648a812b08d764f066cb4c
Harika.padamata <harika.actimize@gmail.com>
2024-12-27 12:40:43 +0530

    build file changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -49,12 +49,15 @@ export default function Login() {
    
   
   useEffect(()=>{
+    checkSession();
+  },[])
+  
+  const checkSession = () => {
     const token  = localStorage.getItem("studentManagment");
     if(token){
       navigate("/");
     }
-  },[])
-  
+  }
 
   return (
     <Formik
