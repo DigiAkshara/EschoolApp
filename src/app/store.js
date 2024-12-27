83c8e68fd78d1cc59065bae7b037b758a9862fe7
Harika.padamata <harika.actimize@gmail.com>
2024-12-27 12:03:56 +0530

    file upload changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,23 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import TenantConfigSlice from "./reducers/TenantConfigSlice";
 import appConfigSlice from "./reducers/appConfigSlice";
-<<<<<<< Updated upstream
 import examSlice from "./reducers/examSlice";
 import studentSlice from "./reducers/studentSlice";
-=======
-import examSlice from "./reducers/examSlice"
 import classReducer from "./reducers/classSlice"
->>>>>>> Stashed changes
 
 export const store = configureStore({
   reducer: {
     tenant: TenantConfigSlice,
     appConfig:appConfigSlice,
     exams:examSlice,
-<<<<<<< Updated upstream
-    students:studentSlice
-=======
+    students:studentSlice,
     class: classReducer,
->>>>>>> Stashed changes
   }
 })
