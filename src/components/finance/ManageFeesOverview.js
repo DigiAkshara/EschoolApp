747e01baf2b706066edfb734e1e8a26cd044855f
amrutha <pamruthaarun@gmail.com>
2025-02-10 12:26:15 +0530

    Dashboard and finance overview static page- updated

--- src/components/finance/ManageFeesOverview.js
+++ src/components/finance/ManageFeesOverview.js
@@ -237,45 +237,7 @@ export default function ManageFeesOverview() {
       
       <div className="flow-root">
         {/* Primary Tabs */}
-        <div>
-            <div className="sm:hidden">
-              <label htmlFor="tabs" className="sr-only">
-                Select a tab
-              </label>
-              {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-              <select
-                id="tabs"
-                name="tabs"
-                defaultValue={tabs.find((tab) => tab.current).name}
-                className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
-              >
-                {tabs.map((tab) => (
-                  <option key={tab.name}>{tab.name}</option>
-                ))}
-              </select>
-            </div>
-            <div className="hidden sm:block">
-              <div className="border-b border-gray-200">
-                <nav aria-label="Tabs" className="-mb-px flex space-x-8">
-                  {tabs.map((tab) => (
-                    <a
-                      key={tab.name}
-                      href={tab.href}
-                      aria-current={tab.current ? 'page' : undefined}
-                      className={classNames(
-                        tab.current
-                          ? 'border-purple-500 text-purple-600'
-                          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                        'whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium',
-                      )}
-                    >
-                      {tab.name}
-                    </a>
-                  ))}
-                </nav>
-              </div>
-            </div>
-        </div>
+        
          
 
         {/* Finaance overview cards */}          
