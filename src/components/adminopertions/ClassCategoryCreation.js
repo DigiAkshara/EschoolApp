c91eccb8f4652c42d3cd4f8bf2619a96947dc5e7
amrutha <pamruthaarun@gmail.com>
2025-02-15 14:55:09 +0530

    Updated Class Category - Admin opetations

---
+++ src/components/adminopertions/ClassCategoryCreation.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import CustomInput from "../../commonComponent/CustomInput";
+import { handleApiResponse } from "../../commonComponent/CommonFunctions";
+import { postData } from "../../app/api";
+import { CLASS_CATEGORIES } from "../../app/url";
+
+const ClassCategoryCreation = ({ onClose , getClasses }) => {
+
+  const [formData, setFormData] = useState({
+    name: "",
+  });
+
+  const getValidationSchema = () => {
+    return Yup.object({
+      name: Yup.string().required(" Class Category is required"),
+    });
+  };
+
+  const handleSubmit = async (values) => {
+    console.log(values);
+    try {
+      const response = await postData(CLASS_CATEGORIES, values);
+      console.log("[RESPONSE]:", response);
+      if (response.status === 200 || response.status === 201) {
+        onClose();
+        getClasses();
+        handleApiResponse(response.data.message, "success");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Formik
+        initialValues={formData}
+        validationSchema={getValidationSchema()}
+        onSubmit={handleSubmit}
+      >
+        {({ values, setFieldValue, errors }) => (
+          <Form>
+            <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-30">
+              <div className="bg-white w-96 rounded-lg shadow-lg">
+                {/* Modal Header */}
+                <div className="flex justify-between items-center bg-purple-600 text-white p-3 rounded-t-lg">
+                  <h2 className="text-lg font-semibold">Add Class Category</h2>
+                  <button
+                    onClick={onClose}
+                    className="text-white hover:text-gray-200"
+                  >
+                    <XMarkIcon className="h-6 w-6" />
+                  </button>
+                </div>
+
+                {/* Modal Body */}
+                <div className="p-6">
+                  <CustomInput
+                    name="name"
+                    label="Class Category Name"
+                    placeholder="Enter Categoty"
+                    required={true}
+                  />
+                </div>
+
+                {/* Modal Footer */}
+                <div className="p-3 flex justify-end space-x-2 border-t">
+                  <button
+                    onClick={onClose}
+                    className="px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400"
+                  >
+                    Cancel
+                  </button>
+                  <button className="px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-500">
+                    Add
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default ClassCategoryCreation;
